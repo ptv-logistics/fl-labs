601c7f747153c28758cdd3962b906eab3838eb00
oliverheilig <oliver.heilig@yahoo.de>
2015-03-16 18:46:01 +0100

    update for xserver 1.20

--- index.js
+++ index.js
@@ -4,58 +4,72 @@ if (!token)
 var hour = 18;
 var enableSpeedPatterns = true;
 var enableRestrictionZones = true;
+var enableTrafficIncidents = true;
+var enableTruckAttributes = true;
 var itineraryLanguage = 'EN';
 var routingProfile = 'truckfast';
 
-var map = L.map('map', { 
-                contextmenu: true,
-                contextmenuWidth: 200,
-                contextmenuItems: [{
-                    text: 'Add Waypoint At Start',
-                    callback: function(ev) {routingControl.spliceWaypoints(0, 0, ev.latlng);}
-                },{
-                    text: 'Add Waypoint At End',
-                    callback: function(ev) {routingControl.spliceWaypoints(routingControl._plan._waypoints.length, 0, ev.latlng);}
-                }]});
+var map = L.map('map', {});
 
 var attribution = '<a href="http://www.ptvgroup.com">PTV</a>, TOMTOM';
 var cluster = 'eu-n-test';
-
-//add tile layer
-var bgLayer = new L.PtvLayer.Tiled("https://xmap-" + cluster + ".cloud.ptvgroup.com", {
-    token: token, attribution: attribution, beforeSend: function (request) {
-        request.callerContext.properties[0] = { key: "Profile", value: "silkysand-bg" };
-
-        if (map.getZoom() < 10) // don't display feature layer for low zoom levels
-            return request;
-
-        request.mapParams.referenceTime = "2014-01-07T" + ((hour == 24) ? '00' : (hour >= 10) ? hour : '0' + hour) + ":00:00+02:00";
-        request.callerContext.properties.push({
-            key: "ProfileXMLSnippet",
-            value: buildProfile()
-        });
-
-        return request;
-    }
-}).addTo(map);
 
 // create a separate pane for the xmap labels, so they are displayed on top of the route line
 // http://bl.ocks.org/rsudekum/5431771
 map._panes.labelPane = map._createPane('leaflet-top-pane', map.getPanes().shadowPane);
 
-// add (non-tiled) label layer
-var fgLayer = new L.NonTiledLayer.WMS("https://xmap-" + cluster + ".cloud.ptvgroup.com" + '/WMS/WMS?xtok=' + token, {
-    opacity: 1.0,
-    layers: 'xmap-silkysand-fg',
-    format: 'image/png',
-    transparent: true,
-    attribution: attribution,
-	pane: map._panes.labelPane
-}).addTo(map);
+
+var getLayers = function(profile) {
+
+//add tile layer
+    var bgLayer = new L.PtvLayer.FeatureLayerBg("https://xmap-eu-n-test.cloud.ptvgroup.com", {
+        token: token,
+        attribution: attribution,
+        profile: profile + "-bg",
+        beforeSend2: function(request) {
+//            request.mapParams.referenceTime = "2014-01-07T" + ((hour == 24) ? '00' : (hour >= 10) ? hour : '0' + hour) + ":00:00+02:00";
+        }
+    });
+
+//add fg layer
+    var fgLayer = new L.PtvLayer.FeatureLayerFg("https://xmap-eu-n-test.cloud.ptvgroup.com", {
+        token: token,
+        attribution: attribution,
+        profile: profile + "-fg",
+        pane: map._panes.labelPane,
+        beforeSend2: function (request) {
+//            request.mapParams.referenceTime = "2014-01-07T" + ((hour == 24) ? '00' : (hour >= 10) ? hour : '0' + hour) + ":00:00+02:00";
+        }
+    });
+
+    return L.layerGroup([bgLayer, fgLayer]);
+}
+
+var incidents = new L.PtvLayer.FeatureLayer({ name: 'PTV_TrafficIncidents' }).addTo(map);
+var restrictionZones = new L.PtvLayer.FeatureLayer({ name: 'PTV_RestrictionZones' }).addTo(map);
+var truckAttributes = new L.PtvLayer.FeatureLayer({ name: 'PTV_TruckAttributes' }).addTo(map);
+var speedPatterns = new L.PtvLayer.FeatureLayer({ name: 'PTV_SpeedPatterns' }).addTo(map);
+//var preferredRoutes = new L.PtvLayer.FeatureLayer({ name: 'PTV_PreferredRoutes' }).addTo(map);
+
+var baseLayers = {
+    "PTV classic": getLayers("ajax"),
+    "PTV sandbox": getLayers("sandbox").addTo(map),
+    "PTV silkysand": getLayers("silkysand"),
+    "PTV gravelpit": getLayers("gravelpit")
+};
+
+L.control.layers(baseLayers, {
+    "Incidents": incidents,
+    "Restriction Zones": restrictionZones,
+    "Truck Attributes": truckAttributes,
+    "Speed Patterns": speedPatterns
+}, { position: 'bottomleft' }).addTo(map);
 
 $('#range').attr("value", hour);
 $('#enableSpeedPatterns').attr("checked", enableSpeedPatterns);
 $('#enableRestrictionZones').attr("checked", enableRestrictionZones);
+$('#enableTrafficIncidents').attr("checked", enableTrafficIncidents);
+$('#enableTruckAttributes').attr("checked", enableTruckAttributes);
 $('#languageSelect').val(itineraryLanguage);
 $('#routingProfile').val(routingProfile);
 
@@ -63,10 +77,12 @@ var sidebar = L.control.sidebar('sidebar').addTo(map);
 sidebar.open("home");
 
 var buildProfile = function () {
-    var template = '<Profile xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"><FeatureLayer majorVersion=\"1\" minorVersion=\"0\"><GlobalSettings enableTimeDependency=\"true\"/><Themes><Theme id=\"PTV_RestrictionZones\" enabled=\"{enableRestrictionZones}\" priorityLevel=\"0\"></Theme><Theme id=\"PTV_SpeedPatterns\" enabled=\"{enableSpeedPatterns}\" priorityLevel=\"0\"/></Themes></FeatureLayer><Routing majorVersion=\"2\" minorVersion=\"0\"><Course><AdditionalDataRules enabled=\"true\"/></Course></Routing></Profile>'
+    var template = '<Profile xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"><FeatureLayer majorVersion=\"1\" minorVersion=\"0\"><GlobalSettings enableTimeDependency=\"true\"/><Themes><Theme id=\"PTV_RestrictionZones\" enabled=\"{enableRestrictionZones}\" priorityLevel=\"0\"></Theme><Theme id=\"PTV_SpeedPatterns\" enabled=\"{enableSpeedPatterns}\" priorityLevel=\"0\"/><Theme id=\"PTV_TrafficIncidents\" enabled=\"{enableTrafficIncidents}\" priorityLevel=\"0\"/><Theme id=\"PTV_TruckAttributes\" enabled=\"{enableTruckAttributes}\" priorityLevel=\"0\"/><Theme id=\"PTV_TimeZones\" enabled=\"true\" priorityLevel=\"0\"/></Themes></FeatureLayer><Routing majorVersion=\"2\" minorVersion=\"0\"><Course><AdditionalDataRules enabled=\"true\"/></Course></Routing></Profile>'
 
     template = template.replace("{enableRestrictionZones}", enableRestrictionZones);
     template = template.replace("{enableSpeedPatterns}", enableSpeedPatterns);
+    template = template.replace("{enableTruckAttributes}", enableTruckAttributes);
+    template = template.replace("{enableTrafficIncidents}", enableTrafficIncidents);
 
     return template;
 }
@@ -75,11 +91,13 @@ var updateParams = function (refreshFeatureLayer) {
     hour = $('#range').val();
     enableSpeedPatterns = $('#enableSpeedPatterns').is(':checked');
     enableRestrictionZones = $('#enableRestrictionZones').is(':checked');
+    enableTruckAttributes = $('#enableTruckAttributes').is(':checked');
+    enableTrafficIncidents = $('#enableTrafficIncidents').is(':checked');
     itineraryLanguage = $('#languageSelect option:selected').val();
     routingProfile = $('#routingProfile option:selected').val();
 
     if (refreshFeatureLayer) {
-        bgLayer.redraw();
+        speedPatterns.redraw();
     }
 
     routingControl.route();
@@ -90,12 +108,12 @@ var routingControl = L.Routing.control({
 		L.latLng(48.813194201165274, 9.2841339111328125),
 		L.latLng(48.694133170886325, 9.122772216796875)
     ], {
-            createMarker: function (i, wp) {
-                return L.marker(wp.latLng, {
-                    draggable: true,
-                    icon: new L.Icon.Label.Default({ labelText: String.fromCharCode(65 + i) })
-                });
-            },
+        createMarker: function (i, wp) {
+            return L.marker(wp.latLng, {
+                draggable: true,
+                icon: new L.Icon.Label.Default({ labelText: String.fromCharCode(65 + i) })
+            });
+        },
         geocoder: L.Control.Geocoder.ptv({ token: token })
     }),
     lineOptions: {
@@ -110,10 +128,10 @@ var routingControl = L.Routing.control({
     },
     router: L.Routing.ptv({
         token: token, beforeSend: function (request) {
-            request.options.push({
-                parameter: "START_TIME",
-                value: "2014-01-07T" + ((hour == 24) ? '00' : (hour >= 10) ? hour : '0' + hour) + ":00:00+02:00"
-            });
+            //request.options.push({
+            //    parameter: "START_TIME",
+            //    value: "2014-01-07T" + ((hour == 24) ? '00' : (hour >= 10) ? hour : '0' + hour) + ":00:00+02:00"
+            //});
 
             request.options.push({
                 parameter: "ROUTE_LANGUAGE",
@@ -134,6 +152,6 @@ var routingControl = L.Routing.control({
     routeDragInterval: 1000
 }).addTo(map);
 
-	routingControl.on('routingerror', function(e){ 
-		alert(e.error.responseJSON.errorMessage);
-	});
+routingControl.on('routingerror', function (e) {
+    alert(e.error.responseJSON.errorMessage);
+});
