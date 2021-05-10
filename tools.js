c97bf03bdd2258c07622f7e0589dfae626de8d1d
oliverheilig <oliver.heilig@yahoo.de>
2021-05-10 17:49:35 +0200

    added includes polyfill

--- tools.js
+++ tools.js
@@ -25,3 +25,18 @@ var runRequest = function (url, request, token, handleSuccess, handleError) {
         }
     });
 };
+
+if (!String.prototype.includes) {
+    String.prototype.includes = function(search, start) {
+        'use strict';
+        if (typeof start !== 'number') {
+            start = 0;
+        }
+
+        if (start + search.length > this.length) {
+            return false;
+        } else {
+            return this.indexOf(search, start) !== -1;
+        }
+    };
+}
