cede626b34346ec167bafbba807ba697dd623c23
Oliver Heilig <oliver.heilig@yahoo.de>
2023-03-05 15:28:28 +0100

    Update token.js

--- token.js
+++ token.js
@@ -1,2 +1,2 @@
 // This token is valid until 2022-06-25T05:02:28Z and was auto-generated @ 2022-04-21T21:39:19Z.
-var token = '742DEE87-F0C7-4F71-ADF2-4BF6E3C48270';
+var token = 'B2B52B12-5271-4079-B0A3-E532C0CD1867';
